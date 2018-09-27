6c6f90f5d21f205c28071f63908bf70371974cbd
kevin <zhangzhubao@gzhaochuan.com>
2018-09-27 09:48:12 +0800

    修复StringFormat工具类报错问题

--- JLib/src/main/java/com/jewel/lib/StringFormat.java
+++ JLib/src/main/java/com/jewel/lib/StringFormat.java
@@ -1,6 +1,6 @@
 package com.jewel.lib;
 
-import android.content.res.Resources;
+import android.content.Context;
 import android.support.annotation.StringRes;
 
 public final class StringFormat {
@@ -12,16 +12,14 @@ public final class StringFormat {
     /**
      * 从字符串资源中获取字符串
      */
-    public static String get(@StringRes int stringRes) {
-        Resources resources = Resources.getSystem();
-        return resources.getString(stringRes);
+    public static String get(Context context, @StringRes int stringRes) {
+        return CompatUtil.getString(context, stringRes);
     }
 
     /**
      * 从字符串资源中获取特殊字符串并格式化
      */
-    public static String get(@StringRes int stringRes, Object... args) {
-        Resources resources = Resources.getSystem();
-        return String.format(resources.getString(stringRes), args);
+    public static String get(Context context, @StringRes int stringRes, Object... args) {
+        return String.format(CompatUtil.getString(context, stringRes), args);
     }
 }
