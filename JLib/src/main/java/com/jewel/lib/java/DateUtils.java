e3b27466f5349195fae96ee4e4be7e88afa63b7e
kevin <zhangzhubao@gzhaochuan.com>
2018-09-28 18:48:03 +0800

    DateUtils新增毫秒转为视频播放时间String的方法

--- JLib/src/main/java/com/jewel/lib/java/DateUtils.java
+++ JLib/src/main/java/com/jewel/lib/java/DateUtils.java
@@ -3,6 +3,8 @@ package com.jewel.lib.java;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Formatter;
+import java.util.Locale;
 
 /**
  * 日期工具
@@ -73,5 +75,25 @@ public class DateUtils {
      */
     public static long dateToLong(Date date) {
         return date.getTime();
+    }
+
+    /**
+     * @param timeMs 要转换为 %d:%02d:%02d 或 %02d:%02d String类型 的时间
+     */
+    public static String timeToString(long timeMs) {
+        if (timeMs <= 0 || timeMs >= 24 * 60 * 60 * 1000) {
+            return "00:00";
+        }
+        long totalSeconds = timeMs / 1000;
+        long seconds = totalSeconds % 60;
+        long minutes = (totalSeconds / 60) % 60;
+        long hours = totalSeconds / 3600;
+        StringBuilder stringBuilder = new StringBuilder();
+        Formatter mFormatter = new Formatter(stringBuilder, Locale.getDefault());
+        if (hours > 0) {
+            return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString();
+        } else {
+            return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+        }
     }
 }
